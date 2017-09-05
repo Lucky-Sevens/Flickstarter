dd09d284fa6743d2ecb5c698772485de52f0e4b9
Pavan Sethi <pavansethi@Pavans-MacBook-Pro.local>
2017-09-05 14:52:22 -0700

    Adds Semantic UI setup

--- db/seeds/genres_seed.js
+++ db/seeds/genres_seed.js
@@ -2,20 +2,33 @@ const models = require('../models');
 
 exports.seed = function (knex, Promise) {
 
-  return models.Genre.where({ genre: 'documentary' }).fetch()
-    .then((genre) => {
-      if (genre) {
-        throw genre;
+  return models.Profile.where({ email: 'admin@domain.com' }).fetch()
+    .then((profile) => {
+      if (profile) {
+        throw profile;
       }
-      return models.Genre.forge({
-        genre: 'documentary'
+      return models.Profile.forge({
+        first: 'System',
+        last: 'Admin',
+        display: 'Administrator',
+        email: 'admin@domain.com'
       }).save();
     })
     .error(err => {
-      console.error('ERROR: failed to create genre');
+      console.error('ERROR: failed to create profile');
       throw err;
     })
+    .then((profile) => {
+      return models.Auth.forge({
+        type: 'local',
+        password: 'admin123',
+        profile_id: profile.get('id')
+      }).save();
+    })
+    .error(err => {
+      console.error('ERROR: failed to create auth');
+    })
     .catch(() => {
-      console.log('WARNING: genre already exists.');
+      console.log('WARNING: defualt user already exists.');
     });
 };
